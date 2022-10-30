c635dfb21c446e94a416e7ddc6f53a3b078d1496
wirededdy <hiamanosei@gmail.com>
2022-10-30 22:12:02 +0000

    "day 10 task 1 done"

--- DAY 10/day-10-start/day-10-start.py
+++ DAY 10/day-10-start/day-10-start.py
@@ -1,3 +1,16 @@
 def isleap(year):
+    that_year = ""
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                that_year = "leap"
+            else:
+                that_year = "not leap"
+        else:
+            that_year = "leap"
+    else:
+        that_year = "not leap"
+    return that_year
 
-    if year
+
+print(isleap(2024))
