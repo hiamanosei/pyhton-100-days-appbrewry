6a30e43d9e2d53b4030ce528a98c9dc4b860f760
wirededdy <hiamanosei@gmail.com>
2022-10-30 23:23:11 +0000

    "day 10 task 1 done"

--- DAY 10/day-10-1-exercise-1/day-10-1-exercise.py
+++ DAY 10/day-10-1-exercise-1/day-10-1-exercise.py
@@ -2,18 +2,23 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year.")
+                that_year = True
             else:
-                print("Not leap year.")
+                that_year = False
         else:
-            print("Leap year.")
+            that_year = True
     else:
-        print("Not leap year.")
+        that_year = False
+    return that_year
 
 
-def days_in_month():
+def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+    if is_leap(year):
+        month_days[1] = 29
+
+    return month_days[month-1]
 
 # 🚨 Do NOT change any of the code below
 year = int(input("Enter a year: "))
