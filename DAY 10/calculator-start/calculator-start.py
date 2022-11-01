3664d1a93e2b35a0b3e7a3225d16bdf2af46fe3c
wirededdy <hiamanosei@gmail.com>
2022-11-01 01:08:59 +0000

    "day 10 task 1 done"

---
+++ DAY 10/calculator-start/calculator-start.py
@@ -0,0 +1,16 @@
+
+operand = {
+    "+": "add",
+    "-": "subtract",
+    "*": "multiply",
+    "/":"divide",
+}
+# value1 = float(input("first digit"))
+# value2 = float(input("Second digit"))
+# sign = input("What is the operator +,-,*,/")
+def calc(value1,value2,sign):
+    pass
+
+
+
+#calc(value1=value1,value2=value2,sign=sign)
