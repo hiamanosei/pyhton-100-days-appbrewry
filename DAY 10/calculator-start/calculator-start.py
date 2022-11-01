194529a05e9b31cf8b22ceeae588421e763e13f0
wirededdy <hiamanosei@gmail.com>
2022-11-01 01:29:17 +0000

    "day 10 task 1 done"

--- DAY 10/calculator-start/calculator-start.py
+++ DAY 10/calculator-start/calculator-start.py
@@ -1,32 +1,43 @@
 
+
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
 operand = {
-    "+": "add",
-    "-": "subtract",
-    "*": "multiply",
-    "/":"divide",
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
 }
-def add(num1,num2):
-    return num1 + num2
 
-
-def add(num1,num2):
-    return num1 + num2
-
-def add(num1,num2):
-    return num1 + num2
-
-def add(num1,num2):
-    return num1 + num2
-
-
-# value1 = float(input("first digit"))
-# value2 = float(input("Second digit"))
-# sign = input("What is the operator")
+value1 = float(input("first digit"))
+value2 = float(input("Second digit"))
+#
 for key in operand:
     print(f"{key}")
 
-def calc(value1,value2,sign):
-    pass
+sign = input("What is the operator")
+
+operation_guess = operand[sign]
+operation_guess(value2,value1)
+calc = operation_guess(value1, value2)
+print(f"{value1}and{sign}and{value2} is {calc}")
+
 
 
 
