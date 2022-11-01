f771a51e863046938b06db88a9c1879c403a7c79
wirededdy <hiamanosei@gmail.com>
2022-11-01 01:15:24 +0000

    "day 10 task 1 done"

--- DAY 10/calculator-start/calculator-start.py
+++ DAY 10/calculator-start/calculator-start.py
@@ -5,6 +5,20 @@ operand = {
     "*": "multiply",
     "/":"divide",
 }
+def add(num1,num2):
+    return num1 + num2
+
+
+def add(num1,num2):
+    return num1 + num2
+
+def add(num1,num2):
+    return num1 + num2
+
+def add(num1,num2):
+    return num1 + num2
+
+
 # value1 = float(input("first digit"))
 # value2 = float(input("Second digit"))
 # sign = input("What is the operator")
